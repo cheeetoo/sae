2918256b0c859f95b396cc4f2be04ad18e6091bc
Finn Cairns <fjamescairns@gmail.com>
2025-05-04 16:56:32 -0500

    some changes

--- data/get_activations.py
+++ data/get_activations.py
@@ -1,4 +1,5 @@
 import torch
+from huggingface_hub import HfApi
 from transformer_lens import HookedTransformer, utils
 from datasets import load_dataset, Dataset
 from einops import rearrange
@@ -7,20 +8,28 @@ from tqdm.auto import tqdm
 MODEL_NAME = "google/gemma-2-2b"
 DATASET_NAME = "HuggingFaceFW/fineweb"
 DATASET_CONFIG = "sample-10BT"
-OUTPUT_FILE = "pile_activations.pt"
+OUTPUT_FILE = "activations.pt"
 REPO_ID = "cheeetoo/gemma-2-2b-fineweb-l13-acts"
-BS = 40
+BS = 190
 LAYER = 13
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.mps.is_available()
+    else "cpu"
+)
 
 print(f"Loading model {MODEL_NAME}")
-model = HookedTransformer.from_pretrained(MODEL_NAME, device=device, dtype=torch.bfloat16)
+model = HookedTransformer.from_pretrained(
+    MODEL_NAME, device=device, dtype=torch.bfloat16
+)
 model.compile()
 model.cfg.n_ctx = model.cfg.n_ctx // 32
 
 print(f"Loading dataset {DATASET_NAME} ({DATASET_CONFIG})")
-dataset = load_dataset(DATASET_NAME, DATASET_CONFIG, split="train[:1%]")
+dataset = load_dataset(DATASET_NAME, DATASET_CONFIG, split="train[:20%]")
 
 
 dataloader = torch.utils.data.DataLoader(
@@ -41,7 +50,7 @@ for batch in tqdm(dataloader):
         model.run_with_hooks(
             toks,
             fwd_hooks=[(utils.get_act_name("resid_post", LAYER), hook)],
-            clear_contexts=True
+            clear_contexts=True,
         )
     torch.cuda.empty_cache()
 
@@ -61,19 +70,19 @@ torch.save(activations_tensor, OUTPUT_FILE)
 print(f"Dataset saved with {len(activations_tensor)} activation vectors.")
 
 print(f"Uploading activations to Hugging Face Hub at '{REPO_ID}'...")
-hf_dataset = Dataset.from_dict(
-    {
-        "activations": activations_tensor.detach().cpu().numpy(),
-        "d_model": [n],
-        "model_name": [MODEL_NAME],
-        "layer": [LAYER],
-        "dataset_name": [DATASET_NAME],
-    }
+api = HfApi()
+
+api.create_repo(
+    repo_id=REPO_ID,
+    repo_type="dataset",
+    exists_ok=True,
 )
 
-hf_dataset.push_to_hub(
+api.upload_file(
+    path_or_fileobj=OUTPUT_FILE,
+    path_in_repo=OUTPUT_FILE,
     repo_id=REPO_ID,
-    commit_message=f"Upload activations from {MODEL_NAME} layer {LAYER}",
+    repo_type="dataset",
 )
 
 print("✅ Upload to Hugging Face Hub complete!")
